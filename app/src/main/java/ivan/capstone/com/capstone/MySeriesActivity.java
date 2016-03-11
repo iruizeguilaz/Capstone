f2e1c5c4fb685eb22f2ab74d5ec0569f6904b148
iruizeguilaz <iruizeguilaz@gmail.com>
2016-03-11 11:37:02 +0100

    remove unsed menu.

--- app/src/main/java/ivan/capstone/com/capstone/MySeriesActivity.java
+++ app/src/main/java/ivan/capstone/com/capstone/MySeriesActivity.java
@@ -57,29 +57,6 @@ public class MySeriesActivity extends AppCompatActivity implements MySeriesFragm
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    @Override
     public void onItemSelected(Serie value, ImageView imageView) {
         if (mTwoPane) {
             Bundle args = new Bundle();
